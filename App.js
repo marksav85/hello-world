7cb4c0317ab31ddb6e9816b6887000d0a61a8217
Mark Saville <mark.saville@outlook.com>
2023-09-23 17:14:24 -0700

    add text input and alert button

--- App.js
+++ App.js
@@ -1,20 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, TextInput, View } from "react-native";
+import { useState } from "react";
+import { Alert, Button, ScrollView } from "react-native";
 
-export default function App() {
+const App = () => {
+  const [text, setText] = useState("");
+  // alert the user input (`text` state's value)
+  const alertMyText = () => {
+    Alert.alert(text);
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput
+        style={styles.textInput}
+        value={text}
+        onChangeText={setText}
+        placeholder="Type Something Here"
+      />
+      <Text style="textDisplay">You wrote: {text}</Text>
+      <Button
+        onPress={() => {
+          alertMyText();
+        }}
+        title="Press Me"
+      />
+      <View>
+        <Text style={{ fontSize: 110 }}>
+          This text is so big! And so long! You have to scroll!
+        </Text>
+      </View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  textInput: {
+    width: "88%",
+    borderWidth: 1,
+    height: 50,
+    padding: 10,
+  },
+  textDisplay: {
+    height: 50,
+    lineHeight: 50,
   },
 });
+
+export default App;
